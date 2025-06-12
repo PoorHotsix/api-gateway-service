18d9ab203585190c4124f0d57816c279e753bd83
JHPush <qnrntmvls@gmail.com>
2025-06-12 19:17:16 +0900

    통합 테스트

--- src/main/java/com/inkcloud/api_gateway_server/config/SecurityConfig.java
+++ src/main/java/com/inkcloud/api_gateway_server/config/SecurityConfig.java
@@ -14,25 +14,21 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http){
-        
         http
-            .cors(withDefaults()) 
+            .cors(withDefaults())
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
             .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-            .authorizeExchange(auth ->auth
-                .pathMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll() // 이 줄 추가!
+            .authorizeExchange(auth -> auth
+                .pathMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
                 .pathMatchers("/api/v1/members/signup/**", "/api/v1/members/login/**", "/api/v1/members/password/**").permitAll()
                 // .pathMatchers("/api/v1/members/**").permitAll()
                 .pathMatchers("/api/v1/payments/webhook").permitAll()
                 // .pathMatchers("/api/v1/orders/**").permitAll()
-                .pathMatchers("/api/v1/products/**").permitAll()
-                .pathMatchers("/api/v1/categories/**").permitAll()
+                .pathMatchers("/api/v1/reviews/products/**").permitAll() // 책별 리뷰 조회는 토큰 없이 허용
+                .pathMatchers("/api/v1/products/**", "/api/v1/categories/**").permitAll() // 책별 리뷰 조회는 토큰 없이 허용
                 .anyExchange().authenticated()
-
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-
         return http.build();
-
     }
 }
