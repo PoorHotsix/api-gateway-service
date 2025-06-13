1c9ae0937777f53ea35a73bcc21f6f3cf145286c
PoorHotsix <majch98@gmail.com>
2025-06-13 14:08:34 +0900

    머지 전 커밋

--- src/main/java/com/inkcloud/api_gateway_server/config/SecurityConfig.java
+++ src/main/java/com/inkcloud/api_gateway_server/config/SecurityConfig.java
@@ -13,13 +13,33 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+    // @Bean
+    // public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+    //     return http
+    //             .csrf(ServerHttpSecurity.CsrfSpec::disable)
+    //             .authorizeExchange(exchange -> exchange
+    //                     .anyExchange().permitAll()
+    //             )
+    //             .build();
+    // }
+
     @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
-        return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange -> exchange
-                        .anyExchange().permitAll()
-                )
-                .build();
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http){
+        
+        http
+            .cors(withDefaults()) 
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+            .authorizeExchange(auth ->auth
+                .pathMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll() // 이 줄 추가!
+                .pathMatchers("/api/v1/members/signup/**", "/api/v1/members/login/**", "/api/v1/members/password/**", "/api/v1/products/**", 
+                "/api/v1/categories/**", "/api/v1/orders/**").permitAll()
+                .anyExchange().authenticated()
+
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+
     }
 }
